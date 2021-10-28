c60e67d339420a23f4c01635fbc29236cd3b8407
Krysenkova <Ekaterina.Krysenkova@student.htw-berlin.de>
2021-10-28 18:27:20 +0200

    Initial commit

---
+++ src/main/java/com/example/kbeproject/KbeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.kbeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KbeProjectApplication.class, args);
+    }
+
+}
