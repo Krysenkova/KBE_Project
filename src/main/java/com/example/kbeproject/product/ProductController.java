ddaac83690f6e8565927de2c1f4a0cda019a277c
Krysenkova <Ekaterina.Krysenkova@student.htw-berlin.de>
2022-01-04 19:15:48 +0100

    save data in csv file

--- src/main/java/com/example/kbeproject/product/ProductController.java
+++ src/main/java/com/example/kbeproject/product/ProductController.java
@@ -1,16 +1,15 @@
 package com.example.kbeproject.product;
 
 import com.example.kbeproject.models.*;
+import com.example.kbeproject.utils.CsvWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.supercsv.io.CsvBeanWriter;
-import org.supercsv.io.ICsvBeanWriter;
-import org.supercsv.prefs.CsvPreference;
 
-import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -35,8 +34,7 @@ public class ProductController {
     public ResponseList sendProducts(@RequestBody List<Product> products) {
         return productService.sendProducts(products);
     }
-
-
+    
     @GetMapping("/delivery_info")
     public DeliveryInfoList getDeliveryInfo() {
         return productService.getDeliveryInfo();
@@ -48,38 +46,23 @@ public class ProductController {
     }
 
     @GetMapping("/export")
-    public void exportToCSV(HttpServletResponse response) throws IOException {
-        response.setContentType("text/csv");
-        String headerKey = "Content-Disposition";
-        String headerValue = "attachment; filename=info.csv";
-        response.setHeader(headerKey, headerValue);
+    public void exportToCSV() throws IOException {
+        CsvWriter writer = new CsvWriter();
         List<Product> l = productService.getProducts();
         ResponseList rl = productService.sendProducts(l);
         DeliveryInfoList dil = productService.getDeliveryInfo();
-        AllInfoList allInfo = new AllInfoList();
+        List<String> stringList = new ArrayList<>();
         for (Product item : rl.getProductList()) {
-            for (Storage stor : dil.getStorageList()) {
-                if (item.getItemId().equals(stor.getItemId())) {
+            for (Storage storage : dil.getStorageList()) {
+                if (item.getItemId().equals(storage.getItemId())) {
                     ProductAllInfo p = new ProductAllInfo(item.getItemId(), item.getName(), item.getDescription(), item.getMaterial(),
                             item.getColour(), item.getWeight(), item.getPriceWithoutVat(), item.getPriceWithVat(),
-                            stor.getDeliveryTime(), stor.getAmount(), stor.getLocation());
-                    allInfo.getAllInfoList().add(p);
+                            storage.getDeliveryTime(), storage.getAmount(), storage.getLocation());
+                    stringList.add(p.toString());
                 }
             }
 
         }
-
-        ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
-        String[] csvHeader = {"Product ID", "Name", "Description", "Material", "Colour", "Weight", "Price Without VAT",
-                                "Price with VAT", "Delivery Time", "Amount", "Location"};
-       /* String[] nameMapping = {"item_id", "name", "description", "material", "colour", "weight", "price_without_vat",
-                                "price_with_vat", "delivery_time", "amount", "product_location"};*/
-
-        csvWriter.writeHeader(csvHeader);
-
-        for (ProductAllInfo prod : allInfo.getAllInfoList()) {
-            csvWriter.write(prod);
-        }
-        csvWriter.close();
+        writer.writeToCsvFile(stringList, new File("all_info.csv"));
     }
 }
